5c7ee7d8819b7a79d19959e4bc2426ba8f976002
DesertDoggy <84849808+DesertDoggy@users.noreply.github.com>
2024-06-12 18:53:07 +0900

    working on main.py and .ini

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import configparser
 import powerlog
 from powerlog import logger,verbose_print, info_print, error_print, variable_str, debug_print
 from . import img2j2k, j2k2pdf,pdf3json,json3pdf,mergejs
+import psutil
 
 
 # コマンドライン引数を解析する
@@ -46,66 +47,59 @@ def main():
     elif args.module == 'merge':
         mergejs.run(args.args)
 
+# .ini [Paths] section
 config = load_config(default_path, settings_path)
-input_folder = config.get('Paths', 'input_folder')
-lossless_folder = config.get('Paths', 'lossless_folder')
-optimized_folder = config.get('Paths', 'optimized_folder')
-origpdf_folder = config.get('Paths', 'origpdf_folder')
-optpdf_folder = config.get('Paths', 'optpdf_folder')
+input_folder = config.get('Paths', 'input_folder') # input_folder for img2j2k.py
+lossless_folder = config.get('Paths', 'lossless_folder') #lossless_folder for img2j2k.py
+optimized_folder = config.get('Paths', 'optimized_folder') #optimized_folder for img2j2k.py
+origpdf_folder = config.get('Paths', 'origpdf_folder') # existing_pdf_folder for mergejs.py --clear
+optpdf_folder = config.get('Paths', 'optpdf_folder') # existing_pdf_folder for mergejs.py
 json_folder = config.get('Paths', 'json_folder')
-textpdf_folder = config.get('Paths', 'textpdf_folder')
-clearpdf_folder = config.get('Paths', 'clearpdf_folder')
-draftpdf_folder = config.get('Paths', 'draftpdf_folder')
-finalpdf_folder = config.get('Paths', 'finalpdf_folder')
+textpdf_folder = config.get('Paths', 'textpdf_folder') # output_folder for json3pdf.py, text_layer_folder for mergejs.py
+clearpdf_folder = config.get('Paths', 'clearpdf_folder') # output_folder for json3pdf.py, text_layer_folder for mergejs.py --clear
+draftpdf_folder = config.get('Paths', 'draftpdf_folder') # output_folder for mergejs.py
+finalpdf_folder = config.get('Paths', 'finalpdf_folder') # output_folder for mergejs.py --clear
 use_system_temp = config.getboolean('Paths', 'use_system_temp')
 divpdf_folder = config.get('Paths', 'divpdf_folder')
 divjson_folder = config.get('Paths', 'divjson_folder')
-tmp_path = config.get('Paths', 'tmp_path')
+tmp_path = config.get('Paths', 'tmp_path') #--temp for img2j2k.py
 imagelog_folder = config.get('Paths', 'imagelog_folder')
 
-skip_conversion_extensions = config.get('image conversion', 'skip_conversion_extensions')
-supported_extensions = config.get('image conversion', 'supported_extensions')
-openjpeg_dll_name = config.get('image conversion', 'openjpeg_dll_name')
+# .ini [image conversion] section
+skip_conversion_extensions = {
+    'skip_conversion_extensions': config.get('image conversion', 'skip_conversion_extensions').split(', ')
+} # skip conversion for pdf compatible images #image_extensions for j2k2pdf.py
+supported_extensions = {
+    'supported_extensions': config.get('image conversion', 'supported_extensions').split(', ')
+} # supported image extensions for pillow
+openjpeg_dll_name = config.get('image conversion', 'openjpeg_dll_name') #openjpeg_dll_name for j2k2pdf.py
+if not config.get('image conversion', 'num_physical_cores') == None:
+    num_physical_cores = config.getint('image conversion', 'num_physical_cores')
+else:
+    num_physical_cores = psutil.cpu_count(logical=False)
+num_threads = num_physical_cores // 2
+DPI = config.getint('image conversion', 'DPI') # --dpi for img2j2k.py
+lossless_conversion_check_mode = config.get('image conversion', 'lossless_conversion_check_mode') # --check,--quick for img2j2k.py
+lossless_conversion = config.getboolean('image conversion', 'lossless_conversion') # Default or --lossless for img2j2k.py
+optimizing_conversion = config.getboolean('image conversion', 'optimizing_conversion') # Default or --optimize for img2j2k.py
 
-[Paths]
-input_folder = ./OriginalImages # input_folder for img2j2k.py
-lossless_folder = ./TEMP/lossless #lossless_folder for img2j2k.py  
-optimized_folder = ./TEMP/optimized #optimized_folder for img2j2k.py
-origpdf_folder = ./OriginalPDF # existing_pdf_folder for mergejs.py --clear
-optpdf_folder = ./OptimizedPDF # existing_pdf_folder for mergejs.py
-json_folder = ./DIjson
-textpdf_folder = ./OCRtextPDF # output_folder for json3pdf.py, text_layer_folder for mergejs.py 
-clearpdf_folder = ./OCRclearPDF # output_folder for json3pdf.py, text_layer_folder for mergejs.py --clear
-draftpdf_folder = ./DraftPDF # output_folder for mergejs.py
-finalpdf_folder = ./OCRfinalPDF # output_folder for mergejs.py --clear
-use_system_temp = False
-divpdf_folder = ./TEMP/divPDF
-divjson_folder = ./TEMP/json
-tmp_path = ./TEMP/tmp #--temp for img2j2k.py
-imagelog_folder = ./TEMP/imagelogs
+# .ini [glymur] section
+glymur_config_path = config.get('glymur', 'glymur_config_path') #glymur_config_path for img2j2k.py
+glymur_threads = config.getint('glymur', 'glymur_threads') #glymur_threads for img2j2k.py
 
-[image conversion]
-skip_conversion_extensions = .j2c, .j2k, .jpc, .jp2, .jpf, .jpg, .jpeg, .jpm, .jpg2, .jpx, .mj2 # skip conversion for pdf compatible images #image_extensions for j2k2pdf.py
-supported_extensions = .bmp, .gif, .j2c, .j2k, .jpc, .jp2, .jpf, .jpg, .jpeg, .jpm, .jpg2, .jpx, .mj2, .png, .psd, .tif, .tiff, .webp # supported image extensions for pillow
-openjpeg_dll_name = openjp2.dll #openjpeg_dll_name for j2k2pdf.py
-num_physical_cores = #psutil.cpu_count(logical=False)
-num_threads = # number of simultaneous images proceessed. #num_physical_cores // 2
-DPI = 600 # --dpi for img2j2k.py
-lossless_conversion_check_mode = slow # --check,--quick for img2j2k.py
-lossless_conversion = True # Default or --lossless for img2j2k.py
-optimizing_conversion = True # Default or --optimize for img2j2k.py
+# .ini [OCR] section
+document_intelligence_credentials_path = config.get('OCR', 'document_intelligence_credentials_path')#document_intelligence_credentials_path for pdf3json.py
+div_mode = config.get('OCR', 'div_mode') #--divide, --pages, --no-divide for pdf3json.py
+default_max_pages = config.getint('OCR', 'default_max_pages') # default_max_pages for pdf3json.py
+max_attemps = config.getint('OCR', 'max_attemps') # --attempts for pdf3json.py
+preserve_partial_files = config.getboolean('OCR', 'preserve_partial_files') # --no-delete for pdf3json.py
 
-
-[glymur]
-glymur_config_path = #os.path.join(os.path.expanduser(~), glymur, glymurrc)
-glyumur_threads = 2 # number of threads to use per image. #glymur.set_option(lib.num_threads, 2)
-
-[OCR]
-document_intelligence_credentials_path = ./diAPI.env
-div_mode = auto #--divide, --pages, --no-divide for pdf3json.py
-default_max_pages = 300 # default_max_pages for pdf3json.py
-max_attemps = 3 # --attempts for pdf3json.py
-preserve_partial_files = False # --no-delete for pdf3json.py
+# .ini [document settings] section
+font_size = config.getint('document settings', 'font_size') #--size for json3pdf.py
+font_threshold = config.get('document settings', 'font_threshold') #--font-threshold for json3pdf.py
+horizontal_font = config.get('document settings', 'horizontal_font') #--hfont for json3pdf.py
+vertical_font = config.get('document settings', 'vertical_font') #--vfont for json3pdf.py
+default_dpi = config.getint('document settings', 'default_dpi') #--dpi for json3pdf.py
 
 
 [document settings]
@@ -127,6 +121,7 @@ horizonatl_adjustment = 0 #--left, --right for mergejs.py
 vertical_adjustment = 0 #--up, --down for mergejs.py
 adjustment_dpi = 600 #--dpi for mergejs.py
 max_pagesize = Blanket #--threshold for mergejs.py
+proccess_num_pages = 50 #--process-pages for mergejs.py
 
 [PageSizes]
 A3 = [842, 1191]
